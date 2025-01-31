37a86643190a26081e05f609a886bcb4d2ae8632
AtenaAzha <azha82.atena@gmail.com>
2025-01-31 04:44:54 +0330

    create oop in supperposition file.

--- superpositon_wave.py
+++ superpositon_wave.py
@@ -2,39 +2,46 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-c = 3e8
-landa1 = 2
-landa2 = 2
-wave_num1 = 2*np.pi / landa1
-wave_num2 = 2*np.pi / landa2
-omega1 = c * wave_num1
-omega2 = c * wave_num2
-domain = 0.5
 
-x_space = np.linspace(0 , 10 , 500)
-t_space = np.linspace(0 , 10 , 100)
+class SuperPosition:
+    def __init__(self):
+        self.c = 3e8
+        self.landa1 = 2
+        self.landa2 = 2
+        self.wave_num1 = 2*np.pi / self.landa1
+        self.wave_num2 = 2*np.pi / self.landa2
+        self.omega1 = self.c * self.wave_num1
+        self.omega2 = self.c * self.wave_num2
+        self.domain = 0.5
 
-
-fig , ax = plt.subplots()
-ax.set_xlim(0, 10)
-ax.set_ylim(-2, 2)
-
-wave1, = ax.plot([], [], color='r', label="Wave 1")
-wave2, = ax.plot([], [], color='b', label="Wave 2")
-super_wave, = ax.plot([], [], color='g', label="Superposition")
-ax.legend()
+    def info(self):
+        self.x_space = np.linspace(0 , 10 , 500)
+        self.t_space = np.linspace(0 , 10 , 100)
 
 
-def moving(t):
-    y1 = domain * np.cos(wave_num1 * x_space - omega1 * t)
-    y2 = domain * np.cos(wave_num2 * x_space - omega2 * t + np.pi / 3)
-    y_sum = y1 + y2
+        self.fig , self.ax = plt.subplots()
+        self.ax.set_xlim(0, 10)
+        self.ax.set_ylim(-2, 2)
 
-    wave1.set_data(x_space , y1)
-    wave2.set_data(x_space , y2)
-    super_wave.set_data(x_space, y_sum)
-    return wave1, wave2, super_wave
+        self.wave1, = self.ax.plot([], [], color='r', label="Wave 1")
+        self.wave2, = self.ax.plot([], [], color='b', label="Wave 2")
+        self.super_wave, = self.ax.plot([], [], color='g', label="Superposition")
+        self.ax.legend()
 
-ani = animation.FuncAnimation(fig, moving, frames=t_space, interval=50, blit=True)
-plt.show()
 
+        def moving(t):
+            y1 = self.domain * np.cos(self.wave_num1 * self.x_space - self.omega1 * t)
+            y2 = self.domain * np.cos(self.wave_num2 * self.x_space - self.omega2 * t + np.pi / 3)
+            y_sum = y1 + y2
+
+            self.wave1.set_data(self.x_space , y1)
+            self.wave2.set_data(self.x_space , y2)
+            self.super_wave.set_data(self.x_space, y_sum)
+            return self.wave1, self.wave2, self.super_wave
+
+        self.ani = animation.FuncAnimation(self.fig, moving, frames=self.t_space, interval=50, blit=True)
+        return plt.show()
+
+
+ins = SuperPosition()
+print(ins.info())
